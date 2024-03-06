73e00a6def2e96119cf147eabf5c6d9bfd33c186
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-03-06 09:53:23 +0200

    change db host and port

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -4,11 +4,13 @@ require('dotenv').config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  url: process.env.ELEPHANT_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  migrationsTransactionMode: 'each',
-  synchronize: false,
   extra: {
     timezone: 'local',
   },
